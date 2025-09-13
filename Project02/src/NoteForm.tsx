cbf161fbc30518c8852b0447f933e45e7f85782e
Hasan <hsmusayev@gmail.com>
2025-09-13 23:56:34 +0400

    feat: tag creation logic

--- Project02/src/NoteForm.tsx
+++ Project02/src/NoteForm.tsx
@@ -3,11 +3,14 @@ import {Button, Col, Form, Row, Stack} from "react-bootstrap"
 import { Link } from "react-router-dom"
 import CreatableReactSelect from "react-select/creatable"
 import type { NoteData, Tag } from "./App"
+import {v4 as uuidV4} from "uuid"
 
 type NoteFormProps = {
     onSubmit: (data: NoteData) => void
+    onAddTag:(tag: Tag) => void
+    availableTags: Tag[]
 }
-export function NoteForm({ onSubmit } : NoteFormProps){
+export function NoteForm({ onSubmit, onAddTag, availableTags } : NoteFormProps){
     const titleRef = useRef<HTMLInputElement>(null)
     const markdownRef = useRef<HTMLTextAreaElement>(null)
     const [selectedTags, setSelectedTags] = useState<Tag[]>([])
@@ -34,7 +37,17 @@ export function NoteForm({ onSubmit } : NoteFormProps){
                     <Col>
                         <Form.Group controlId="tags">
                             <Form.Label>Tags</Form.Label>
-                            <CreatableReactSelect value={selectedTags.map(tag => {
+                            <CreatableReactSelect 
+                                onCreateOption={label => {
+                                    const newTag = {id: uuidV4(), label}
+                                    onAddTag{newTag}
+                                    setSelectedTags(prev => [...prev, newTag])
+                                }}
+                            
+                                value={selectedTags.map(tag => {
+                                return {label: tag.label, value: tag.id}
+                            })}
+                            options={availableTags.map(tag => {
                                 return {label: tag.label, value: tag.id}
                             })}
                              onChange={tags => {
